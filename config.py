fa645ba7de2e27c10cafc1de1b79356795826dee
wil-sys <52981362+wil-sys@users.noreply.github.com>
2024-01-29 02:53:03 -0500

    Added stripped down Config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+#These are settings that cannot be auto-configured efficently due to the nature of the program
+#Make sure to edit these to be accurate to your printer configuration
+MoonrakerURL= "http://192.168.2.46:7125"
+ScreenInterface= /dev/serial0
+ScreenBaud= 115200
